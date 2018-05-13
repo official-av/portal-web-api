539eb28471ac637285b513823cf2f30d1ded1c05
Anmol Vashistha <anmolvashisthaav@gmail.com>
2018-05-13 12:55:03 +0530

    checkusername phonenum return

--- authentication/views.py
+++ authentication/views.py
@@ -108,7 +108,7 @@ class checkUsername(APIView):                       #Check Username
         account=self.get_object(request.data['username'])
         print(account)
         if(account!=None):
-            return Response({'user':account.phonenum},status=status.HTTP_201_CREATED)
+            return Response({'user':account.phonenum.id},status=status.HTTP_201_CREATED)
         else:
             return Response({'user':'does not exists'},status=status.HTTP_400_BAD_REQUEST)
 
